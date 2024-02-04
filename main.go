04d242952436a04aac8fd1dd58763643cb897b83
Erik Näslund <code@nsld.dev>
2024-02-04 19:59:28 +0100

    Serve static CSS
    Also break out routers

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
-import "go-starter/domain/app"
+import (
+	"flag"
+	"fmt"
+	"go-starter/router"
+	"mime"
+	"net/http"
+)
+
+var (
+	host = flag.String("host", "localhost", "Host to listen on")
+	port = flag.Int("port", 8080, "Port to listen on")
+)
 
 func main() {
-	app, err := app.New()
+	flag.Parse()
+	_ = mime.AddExtensionType((".css"), "text/css")
+	router := router.New()
+	addr := fmt.Sprintf("%s:%d", *host, *port)
+	fmt.Printf("Listening on %s...\n", addr)
+	err := http.ListenAndServe(addr, router)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	println("Server listening on " + app.Server.Addr)
-	app.Server.ListenAndServe()
 }
---
+++ router/home.go
@@ -0,0 +1,12 @@
+package router
+
+import (
+	"go-starter/html"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	html.Home(w, html.HomeParams{
+		Title: "Home",
+	}, partial(r))
+}
